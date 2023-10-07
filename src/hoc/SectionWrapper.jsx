f5c007ed69ae9bb9010ea39323b86bda55db42c1
Suyash Shingare <59977948+suyashrs@users.noreply.github.com>
2023-10-06 23:34:33 -0700

    Fix import in SessionWrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { style } from '../styles';
+import { styles } from '../styles';
 import { staggerContainer } from '../utils/motion';
 import { Component } from 'react';
 
