20538d248d53b3bb97a6b02b87d361d6e3f62b3a
suyashrs <59977948+suyashrs@users.noreply.github.com>
2023-10-09 13:28:33 -0700

    add Vertical Experience Timeline

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,9 +18,9 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href="#aboute">
+        <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.dev 
+            <motion.div 
               animate={{
                 y: [0, 24, 0] 
               }}
--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -6,9 +6,17 @@ import { Component } from 'react';
 const SectionWrapper = (Component, idName) => 
 function HOC() {
     return (
-        <motion.div>
+        <motion.section
+            variants={staggerContainer()}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once:true, amount: 0.25 }}
+            className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
+            <span className='hash-span' id={idName}>
+                &nbsp;
+            </span>
             <Component/>
-        </motion.div>
+        </motion.section>
     )
 }
 
