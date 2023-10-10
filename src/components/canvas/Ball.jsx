7fb65796b8d8f5dfe3cb89443b39d02a52916e71
suyashrs <59977948+suyashrs@users.noreply.github.com>
2023-10-10 09:35:47 -0700

    Experience section complete

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {Suspense} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture[(props.imgUrl)];
+
   return (
     <div>Ball</div>
   )
 }
+
+const BallCanvas =  
 
 export default Ball
